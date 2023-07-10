7cb5ad1cdcbb7bdf12a0f8cff499e9ade6947e87
User <ol.shink87@gmail.com>
2023-07-10 21:15:26 +0300

    push .gitignore

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+task1();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+        int[]arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        float[] arr1 = {1.57f, 7.654f, 9.986f};
+        String [] strArr = {"I", "LOVE", "learning java"};
+    }
+
+}
