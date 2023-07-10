77628286a10930423255a1dc37294ae755540cf0
User <ol.shink87@gmail.com>
2023-07-10 22:08:19 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         arr[2] = 3;
         float[] arr1 = {1.57f, 7.654f, 9.986f};
         String[] strArr = {"I", "LOVE", "learning java"};
-        for (int i = 0; i < arr.length; i++) {
+                for (int i = 0; i < arr.length; i++) {
             if (i == arr.length - 1) {
                 System.out.println(arr[i]);
             } else {
@@ -30,6 +30,28 @@ public class Main {
                 System.out.println(strArr[k]);
             } else {
                 System.out.print(strArr[k] + ", ");
+            }
+        }
+        System.out.println("Задача 3");
+        for (int a = arr.length-1; a >= 0; a--) {
+            if (a == 0) {
+                System.out.println(arr[a]);
+            } else {
+                System.out.print(arr[a] + ", ");
+            }
+        }
+        for (int r = arr1.length-1; r >=0; r--) {
+            if (r == 0) {
+                System.out.println(arr1[r]);
+            } else {
+                System.out.print(arr1[r] + ", ");
+            }
+        }
+        for (int h = strArr.length-1; h >= 0; h--) {
+            if (h == 0) {
+                System.out.println(strArr[h]);
+            } else {
+                System.out.print(strArr[h] + ", ");
             }
         }
     }
