0e0007aeaa135e98ec882996c9887682c21f9263
User <ol.shink87@gmail.com>
2023-07-10 22:13:56 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
     }
-
     public static void task1() {
         System.out.println("Задача 1 - 2");
         int[] arr = new int[3];
@@ -11,7 +12,7 @@ public class Main {
         arr[2] = 3;
         float[] arr1 = {1.57f, 7.654f, 9.986f};
         String[] strArr = {"I", "LOVE", "learning java"};
-                for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (i == arr.length - 1) {
                 System.out.println(arr[i]);
             } else {
@@ -33,27 +34,35 @@ public class Main {
             }
         }
         System.out.println("Задача 3");
-        for (int a = arr.length-1; a >= 0; a--) {
+        for (int a = arr.length - 1; a >= 0; a--) {
             if (a == 0) {
                 System.out.println(arr[a]);
             } else {
                 System.out.print(arr[a] + ", ");
             }
         }
-        for (int r = arr1.length-1; r >=0; r--) {
+        for (int r = arr1.length - 1; r >= 0; r--) {
             if (r == 0) {
                 System.out.println(arr1[r]);
             } else {
                 System.out.print(arr1[r] + ", ");
             }
         }
-        for (int h = strArr.length-1; h >= 0; h--) {
+        for (int h = strArr.length - 1; h >= 0; h--) {
             if (h == 0) {
                 System.out.println(strArr[h]);
             } else {
                 System.out.print(strArr[h] + ", ");
             }
         }
+        System.out.println("Задача 4");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] = arr[i] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
 
